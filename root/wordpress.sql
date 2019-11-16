ed53ff6b7940750f6c65a1b653cc82ba4bb9c4e0
Christer Edwards <christer.edwards@gmail.com>
2019-11-16 11:55:34 -0700

    removed secondary sql file

--- root/wordpress.sql
+++
@@ -1,7 +0,0 @@
-/*
-This file used to bootstrap wordpress database.
-It is deleted at template completion.
-*/
-CREATE DATABASE wordpress;
-CREATE USER wpuser@localhost IDENTIFIED BY 'password_here';
-GRANT ALL PRIVILEGES ON wordpress.* TO wpuser@localhost;
