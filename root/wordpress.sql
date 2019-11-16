be8f7702414f2ba60fe271330f04fd5bdb787509
Christer Edwards <christer.edwards@gmail.com>
2019-11-16 07:25:03 -0700

    password replace automation

--- root/wordpress.sql
+++ root/wordpress.sql
@@ -3,5 +3,5 @@ This file used to bootstrap wordpress database.
 It is deleted at template completion.
 */
 CREATE DATABASE wordpress;
-CREATE USER wpuser@localhost IDENTIFIED BY 'p@ssw0rd';
+CREATE USER wpuser@localhost IDENTIFIED BY 'password_here';
 GRANT ALL PRIVILEGES ON wordpress.* TO wpuser@localhost;
