1cfac7096983a6f09f10de1f1fc603edce552e92
Thijs van der Woude <thijsvanderwoude99@gmail.com>
2022-03-15 13:18:04 +0100

    Use Unix socket instead of TCP

--- root/bootstrap-wp.sh
+++ root/bootstrap-wp.sh
@@ -9,16 +9,16 @@ BOOTSTRAP_TOKEN=$(openssl rand -hex 18)
 cp /usr/local/www/wordpress/wp-config-sample.php /usr/local/www/wordpress/wp-config.php
 
 ## find/replace magic
-sed -i '' -e s#username_here#wpuser# \
+sed -i '' -e s#username_here#wp_user# \
   -e s#password_here#$BOOTSTRAP_TOKEN# \
   -e s#database_name_here#wordpress# \
     /usr/local/www/wordpress/wp-config.php
 
-sed -i '' 's/localhost/127.0.0.1/' /usr/local/www/wordpress/wp-config.php
+sed -i '' 's/localhost/localhost:/var/run/mysql/mysql.sock' /usr/local/www/wordpress/wp-config.php
 
 ## import database
-echo "CREATE DATABASE wordpress; CREATE USER wpuser@localhost IDENTIFIED BY
-'$BOOTSTRAP_TOKEN'; GRANT ALL PRIVILEGES ON wordpress.* TO wpuser@localhost;" | mysql
+echo "CREATE DATABASE wordpress; CREATE USER wp_user@localhost IDENTIFIED BY
+'$BOOTSTRAP_TOKEN'; GRANT ALL PRIVILEGES ON wordpress.* TO wp_user@localhost;" | mysql
 
 ## cleanup
 rm /root/bootstrap-wp.sh
